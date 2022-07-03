2834d8574f684767e26b5caed889236d261320ef
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2022-07-03 17:26:00 -0400

    test

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,12 +1,15 @@
 import { render, fireEvent } from "@testing-library/react";
 import Counter from "./Counter";
 
-describe(Counter, () => {
+describe("Counter", () => {
+
   it("counter displays correct initial count", () => {
     const { getByTestId } = render(<Counter initialCount={0} />);
     const countValue = Number(getByTestId("count").textContent);
     expect(countValue).toEqual(0);
   });
+
+
 
   it("count should increment by 1 if the increment button is clicked", () => {
     const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
@@ -18,6 +21,8 @@ describe(Counter, () => {
     expect(countValue2).toEqual(1);
   });
 
+
+
   it("count should decrement by 1 if the decrement button is clicked", () => {
     const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
     const decrementBttn = getByRole("button", { name: "Decrement" });
@@ -25,6 +30,8 @@ describe(Counter, () => {
     fireEvent.click(decrementBttn);
     expect(Number(getByTestId("count").textContent)).toEqual(-1);
   });
+
+
 
   it("count should be 0 if the restart button is clicked", () => {
     const { getByTestId, getByRole } = render(<Counter initialCount={50} />);
@@ -34,6 +41,8 @@ describe(Counter, () => {
     expect(Number(getByTestId("count").textContent)).toEqual(0);
   });
 
+
+
   it("count should invert signs if the switch signs button is clicked", () => {
     const { getByTestId, getByRole } = render(<Counter initialCount={50} />);
     const switchBttn = getByRole("button", { name: "Switch Signs" });
@@ -41,4 +50,6 @@ describe(Counter, () => {
     fireEvent.click(switchBttn);
     expect(Number(getByTestId("count").textContent)).toEqual(-50);
   });
+
+  
 });
